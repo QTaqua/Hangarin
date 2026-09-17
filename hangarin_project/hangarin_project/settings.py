cc65c66c4091c4568ea00eba01315a079f9c0090
Jabriel Encarnacion <163326580+QTaqua@users.noreply.github.com>
2026-09-17 17:49:52 +0800

    Update login and logout URL settings

--- hangarin_project/hangarin_project/settings.py
+++ hangarin_project/hangarin_project/settings.py
@@ -134,9 +134,12 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+SITE_ID = 1
+
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/login/'
-LOGIN_URL = '/login/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
+LOGIN_URL = '/accounts/login/
 # ACCOUNT_EMAIL_REQUIRED = True
 # ACCOUNT_USERNAME_REQUIRED = False
 # ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
