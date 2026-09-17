b720b94a113a8a91aa3803e86732c5ef01c1225e
Jabriel Encarnacion <163326580+QTaqua@users.noreply.github.com>
2026-09-17 17:40:11 +0800

    Update ALLOWED_HOSTS for production settings

--- hangarin_project/hangarin_project/settings.py
+++ hangarin_project/hangarin_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9l-!xs$$9m+a8#rczcrdkjiu7=)-ib%i5*1m1z$25fbjvkt)v^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['qtaqua.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
