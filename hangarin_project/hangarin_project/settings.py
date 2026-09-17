410aa4a7947c7b147a20e7ecd2fba51eb817df07
Jabriel Encarnacion <163326580+QTaqua@users.noreply.github.com>
2026-09-17 17:50:40 +0800

    Fix LOGIN_URL syntax in settings.py

--- hangarin_project/hangarin_project/settings.py
+++ hangarin_project/hangarin_project/settings.py
@@ -139,7 +139,7 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
-LOGIN_URL = '/accounts/login/
+LOGIN_URL = '/accounts/login/'
 # ACCOUNT_EMAIL_REQUIRED = True
 # ACCOUNT_USERNAME_REQUIRED = False
 # ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
